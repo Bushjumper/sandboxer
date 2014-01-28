f0af3f2a3b1aa1b681944d38a8467d7b51670c62
Graeme Longman <graemelongman@gmail.com>
2014-01-28 14:29:33 +0200

    Adds setup for testing using Karma test runner, QUnit framework and PhantomJS

---
+++ js/tests/unit_tests.js
@@ -0,0 +1,11 @@
+test('Todo has a title', function() {
+  var todo = Todos.Todo.store.createRecord('todo', {title: 'My first todo'});
+  var title = todo.get('title');
+  equal(title, 'My first todo', "Title was " + title);
+});
+
+test('Todo has a isCompleted boolean', function() {
+  var todo = Todos.Todo.store.createRecord('todo', {isCompleted: false});
+  var isCompleted = todo.get('isCompleted');
+  equal(isCompleted, false, "isCompleted was" + isCompleted);
+});
---
+++ karma.conf.js
@@ -0,0 +1,24 @@
+module.exports = function(karma) {
+  karma.set({
+    basePath: 'js',
+      
+    files: [
+      "libs/jquery-1.10.2.min.js",
+      "libs/handlebars-1.0.0.js",
+      "libs/ember.js",
+      "libs/ember-data.js",
+      "libs/jquery-mockjax/jquery.mockjax.js",
+      "app.js",
+      "router.js",
+      "models/*.js",
+      "tests/*.js"
+    ],
+      
+    logLevel: karma.LOG_ERROR,
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    autoWatch: false,
+       
+    frameworks: ["qunit"]
+  });
+};
